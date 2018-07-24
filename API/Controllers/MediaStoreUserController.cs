dddd5f36c7f55a80a88725dd449a1b481e01c2b5
Igor Moskvitin <mig84@yandex.ru>
2018-07-24 22:52:48 +0700

    API fix

--- API/Controllers/MediaStoreUserController.cs
+++ API/Controllers/MediaStoreUserController.cs
@@ -1,10 +1,10 @@
 ﻿using Amazon;
-using Amazon.IdentityManagement.Model;
 using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System;
-using System.Threading.Tasks;
 
 namespace API.Controllers
 {
@@ -27,46 +27,38 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<UserDto> Post([FromBody] UserDto dto)
+        public Dto Post([FromBody] Dto dto)
         {
             var credentials = new BasicAWSCredentials(AwsOptions.Key, AwsOptions.Password);
-            var response = await CreateIamUserAsync(credentials, dto.Name);
-            return new UserDto
-                {
-                    Name = response.Item1.UserName,
-                    Key = response.Item2.AccessKeyId,
-                    Pass = response.Item2.SecretAccessKey
-                };
-            
-        }
-
-        private static async Task<(User, AccessKey)> CreateIamUserAsync(AWSCredentials credentials, string userName)
-        {
-            using (var client = new Amazon.IdentityManagement.AmazonIdentityManagementServiceClient(credentials,
-                RegionEndpoint.EUCentral1))
+            using (var s3Client = new AmazonS3Client(credentials, RegionEndpoint.EUCentral1))
             {
-                var request = new CreateUserRequest(userName);
-                var response = await client.CreateUserAsync(request);
-                Console.WriteLine($"User {userName} was created.");
+                var key = Guid.NewGuid();
+                var path = "users/" + key + ".ext";
+                var request = new GetPreSignedUrlRequest
+                {
+                    BucketName = "my-test-bucket-coolrocket",
+                    Key = path,
+                    Expires = DateTime.Now.AddHours(1)
+                };
 
-                //create creds for user
-                var createKeyRequest = new CreateAccessKeyRequest {UserName = response.User.UserName};
-                var accessKeyResponse = await client.CreateAccessKeyAsync(createKeyRequest);
-                //add policy to user (demo)
-                var attachUserPolicyRequest = new AttachUserPolicyRequest();
-                attachUserPolicyRequest.UserName = userName;
-                attachUserPolicyRequest.PolicyArn = "arn:aws:iam::aws:policy/AWSElementalMediaStoreFullAccess";
-                await client.AttachUserPolicyAsync(attachUserPolicyRequest);
+                var url = s3Client.GetPreSignedURL(request);
+                var response = new Dto
+                {
+                    FileKey = key.ToString(),
+                    FileExtension = dto.FileExtension,
+                    FileUrl = url
+                };
 
-                return (response.User, accessKeyResponse.AccessKey);
+                return response;
             }
         }
 
-        public class UserDto
+
+        public class Dto
         {
-            public string Name { get; set; }
-            public string Key { get; set; }
-            public string Pass { get; set; }
+            public string FileKey { get; set; }
+            public string FileExtension { get; set; }
+            public string FileUrl { get; set; }
         }
     }
 }
